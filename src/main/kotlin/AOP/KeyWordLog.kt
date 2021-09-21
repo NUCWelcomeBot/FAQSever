3709e454c37584e473960be720037d549080c8be
farewell12345 <1149558764@qq.com>
2021-09-21 22:03:17 +0800

    接入注册中心

---
+++ src/main/kotlin/AOP/KeyWordLog.kt
@@ -0,0 +1,15 @@
+package AOP
+
+
+/**
+ * Controller层统计点击量的自定义注解
+ */
+@Target(
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER,
+    AnnotationTarget.VALUE_PARAMETER
+)
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
+annotation class KeyWordLog(val value: String = "")
