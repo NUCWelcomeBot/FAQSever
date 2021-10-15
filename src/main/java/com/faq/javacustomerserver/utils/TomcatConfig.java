5384431be0ca420c89c75d28e7b2f659bb306a51
farewell12345 <1149558764@qq.com>
2021-10-15 12:19:27 +0800

    去https

--- src/main/java/com/faq/javacustomerserver/utils/TomcatConfig.java
+++ src/main/java/com/faq/javacustomerserver/utils/TomcatConfig.java
@@ -11,29 +11,29 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 class TomcatConfig {
-    @Bean
-    TomcatServletWebServerFactory tomcatServletWebServerFactory() {
-        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory(){
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint constraint = new SecurityConstraint();
-                constraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                constraint.addCollection(collection);
-                context.addConstraint(constraint);
-            }
-        };
-        factory.addAdditionalTomcatConnectors(createTomcatConnector());
-        return factory;
-    }
-    private Connector createTomcatConnector() {
-        Connector connector = new
-                Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
-        connector.setPort(8081);
-        connector.setSecure(false);
-        connector.setRedirectPort(8080);
-        return connector;
-    }
+//    @Bean
+//    TomcatServletWebServerFactory tomcatServletWebServerFactory() {
+//        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory(){
+//            @Override
+//            protected void postProcessContext(Context context) {
+//                SecurityConstraint constraint = new SecurityConstraint();
+//                constraint.setUserConstraint("CONFIDENTIAL");
+//                SecurityCollection collection = new SecurityCollection();
+//                collection.addPattern("/*");
+//                constraint.addCollection(collection);
+//                context.addConstraint(constraint);
+//            }
+//        };
+//        factory.addAdditionalTomcatConnectors(createTomcatConnector());
+//        return factory;
+//    }
+//    private Connector createTomcatConnector() {
+//        Connector connector = new
+//                Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setScheme("http");
+//        connector.setPort(8081);
+//        connector.setSecure(false);
+//        connector.setRedirectPort(8080);
+//        return connector;
+//    }
 }
